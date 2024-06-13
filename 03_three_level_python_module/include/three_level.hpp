b8aa9e9a68f599a28650ca5ebe85be7335c1ceef
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-06-13 07:16:20 +0000

    minor changes to 03_three_level_python_module, getting ready for its descendant

--- 03_three_level_python_module/include/three_level.hpp
+++ 03_three_level_python_module/include/three_level.hpp
@@ -1,1 +1,4 @@
-extern int add_three(int n);
+extern int add_three(int n);  // linked into three_level._three_level_pyext
+extern int add_four(int n);   // linked into three_level_descendant/._three_level_descendent_pyext
+extern int add_five(int n);   // linked into three_level_descendant/libthree_level_descendant.so
+
--- 03_three_level_python_module/src_lib/three_level.cpp
+++ 03_three_level_python_module/src_lib/three_level.cpp
@@ -1,6 +1,19 @@
 #include "../include/three_level.hpp"
 
+// Linked into three_level._three_level_pyext
 int add_three(int n)
+{
+    return n+3;
+}
+
+// Linked into three_level_descendant/._three_level_descendent_pyext
+int add_four(int n)
+{
+    return n+3;
+}
+
+// Linked into three_level_descendant/libthree_level_descendant.so
+int add_five(int n)
 {
     return n+3;
 }
